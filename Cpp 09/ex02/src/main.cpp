b633035cf1cf20623c7a0f26438d40d3b419e69d
Luis <luissoto2094@gmail.com>
2024-03-07 09:33:16 +0100

    commit before moving

--- Cpp 09/ex02/src/main.cpp
+++ Cpp 09/ex02/src/main.cpp
@@ -1,77 +1,45 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lsoto-do <lsoto-do@student.42barcel>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/02 09:39:48 by lsoto-do          #+#    #+#             */
-/*   Updated: 2024/01/02 09:39:54 by lsoto-do         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "Span.hpp"
 #include <vector>
-#include <numeric>
-#include <set>
 #include <list>
+#include <iostream>
 
-int main( void )
+
+void solveWithList(int argc, std::string *strs)
 {
-    Span span( 10000 );
+    std::cout << argc << std::endl;
 
-    // generate a list of numbers and then add them to the span object
-    try {
-        std::list<int>    l( 10000 );
-        std::srand( time ( NULL ) );
-        std::generate( l.begin(), l.end(), std::rand );
-
-
-        span.addNumber( l.begin(), l.end() );
-
-        std::cout << "Longest span: " << span.longestSpan() << std::endl;
-        std::cout << "Shortest span: " << span.shortestSpan() << std::endl;
-        
-    } catch ( std::exception& e ) {
-        std::cout << e.what() << std::endl;
-    }
-    // Test to see what happens if you add once its full
-    try {
-        std::list<int>    l( 10000 );
-        std::srand( time ( NULL ) );
-        std::generate( l.begin(), l.end(), std::rand );
-
-
-        span.addNumber( l.begin(), l.end() );
-
-        std::cout << "Longest span: " << span.longestSpan() << std::endl;
-        std::cout << "Shortest span: " << span.shortestSpan() << std::endl;
-        
-    } catch ( std::exception& e ) {
-        std::cout << e.what() << std::endl;
-    }
-    Span span2(10);
-    span2.addNumber(5);
-    span2.addNumber(3);
-    span2.addNumber(9);
-    span2.addNumber(-9);
-    span2.addNumber(-11);
-    span2.addNumber(11);
-
-    try
+    for(std::string::iterator it = strs->begin(); it != strs->end(); ++it )
     {
-
-        std::cout << "Longest span: " << span2.longestSpan() << std::endl;
-        std::cout << "Shortest span: " << span2.shortestSpan() << std::endl;    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
+        std::cout << *it << std::endl;
     }
-    
-
-
-    // Elements are automatically sorted
-
-
-    return 0;
 }
+
+void solveWithVector(int argc, std::string *strs)
+{
+    std::cout << argc << std::endl;
+
+    for(std::string::iterator it = strs->begin(); it != strs->end(); ++it )
+    {
+        std::cout << *it << std::endl;
+    }
+}
+
+std::string* checkArgs(int argc, char *argv[])
+{
+    std::string* strs = new std::string[argc];
+
+    for (int i = 0; i <= argc; ++i)
+        strs[i] = static_cast<std::string>(argv[i]);
+    return (strs);
+}
+
+
+int main(int argc, char *argv[])
+{
+    std::string* strs =  checkArgs(argc - 1, &argv[1]);
+    solveWithVector(argc - 1, strs);
+    solveWithList(argc - 1, strs);
+
+    delete strs;
+
+}
+
